171f59c6638779899f2d11403ab7e2018ae19411
kuno2582 <kuno9411@gmail.com>
2017-05-05 19:22:09 +0900

    second test

--- DesignPractice/src/com/design/adapter/AdapterPatternDemo.java
+++ DesignPractice/src/com/design/adapter/AdapterPatternDemo.java
@@ -4,7 +4,7 @@ public class AdapterPatternDemo {
 	public static void main(String[] args) {
 		AudioPlayer audioPlayer = new AudioPlayer();
 
-		audioPlayer.play("mp3", "어디에도?.mp3");
+		audioPlayer.play("mp3", "어디에도.mp3");
 		audioPlayer.play("mp4", "그대라는 사치.mp4");
 		audioPlayer.play("vlc", "에일리언.vlc");
 		audioPlayer.play("avi", "야구동영상.avi");
